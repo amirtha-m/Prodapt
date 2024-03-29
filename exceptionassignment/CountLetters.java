27391dff96c0078854d1aaf699ebb3220133ad30
amirtha-m <mathiamirtha96@gmail.com>
2024-03-29 14:17:30 +0530

    my second commit

---
+++ exceptionassignment/CountLetters.java
@@ -0,0 +1,31 @@
+package DaySeven.exceptionassignment;
+
+import java.util.Scanner;
+
+public class CountLetters {
+	public static void main(String[] args) {
+		int[] counts = new int[26];
+		Scanner scan = new Scanner(System.in);
+		// get word from user
+		System.out.print("Enter a single word (letters only, please): ");
+		String word = scan.nextLine();
+		// convert to all upper case
+		word = word.toUpperCase();
+		// count frequency of each letter in string
+		try {
+			for (int i = 0; i < word.length(); i++) {
+				counts[word.charAt(i) - 'A']++;
+			}
+		} catch (ArrayIndexOutOfBoundsException a) {
+			System.out.println("Not a Letter " + a);
+		}
+		// print frequencies
+		System.out.println();
+		for (int i = 0; i < counts.length; i++) {
+			if (counts[i] != 0)
+				;
+			System.out.println((char) (i + 'A') + ":" + counts[i]);
+		}
+	}
+	
+}
---
+++ exceptionassignment/IntegerSum.java
@@ -0,0 +1,25 @@
+package DaySeven.exceptionassignment;
+import java.util.Scanner;
+public class IntegerSum {
+	public static void main(String[] args) {
+		int val,sum=0;
+		Scanner scan=new Scanner(System.in);
+		String line;
+		
+		System.out.println("Enter a line of text: ");
+		Scanner scanLine = new Scanner(scan.nextLine());
+		
+		
+			while(scanLine.hasNext()) {
+				try {
+				val=Integer.parseInt(scanLine.next());
+				sum+=val;
+			}catch(NumberFormatException n) {}
+			
+		}
+		System.out.println("The sum of the line is: "+sum);
+	}
+
+
+
+}
